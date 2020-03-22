c0f832957318362915725a2df0a953274f24f058
Han Wang <hwang@lyft.com>
2020-03-22 00:27:45 -0700

    update

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='adagio',
-    version='0.0.1',
+    name="adagio",
+    version="0.0.1",
     packages=find_packages(),
-    description='A Dag IO Framework',
-    author='Han Wang',
-    author_email='goodwanghan@gmail.com',
-    install_requires=[
-        'pandas'
-    ],
-    extras_require={
-        
-    }
+    description="A Dag IO Framework",
+    author="Han Wang",
+    author_email="goodwanghan@gmail.com",
+    install_requires=["pandas"],
+    extras_require={},
 )
