e0ab21e9d54944c7966e305b35e0bfe32c2514ba
Han Wang <hwang@lyft.com>
2020-03-21 23:52:50 -0700

    Setup repo

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='adagio',
+    version='0.0.1',
+    packages=find_packages(),
+    description='A Dag IO Framework',
+    author='Han Wang',
+    author_email='goodwanghan@gmail.com',
+    install_requires=[
+        'pandas'
+    ],
+    extras_require={
+        "test": [
+            'mypy',
+            'flake8',
+            'autopep8',
+            'pylint',
+            'pytest',
+            'pytest-cov',
+            'pytest-mock'
+        ]
+    }
+)
